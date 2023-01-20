60d0aca348ae2173331c1ef591546fc2f7ca142f
Drew Roby (STL) <drew.roby@daugherty.com>
2023-01-20 17:38:54 -0600

    begin building models

--- app/models/ItemInMemoryModel.scala
+++ app/models/ItemInMemoryModel.scala
@@ -4,15 +4,22 @@ import collection.mutable
 
 // Play didn't like when I made this a class.  Why?
 object ItemInMemoryModel {
-  private val users = mutable.Map[String, String]("asdf" -> "fdsa ")
-
+  private val users = mutable.Map[String, String]("asdf" -> "fdsa")
+  private val items = mutable.Map[String, List[String]]("asdf" -> List("Shirt","Pants","Coat"))
   def validateUser(username: String, password: String): Boolean = {
     users.get(username).map(_ == password).getOrElse(false)
   }
 
-  def createUser(username: String, password: String): Boolean = ???
+  def createUser(username: String, password: String): Boolean = {
+    if (users.contains(username)) false else {
+      users(username) = password
+      true
+    }
+  }
 
-  def getItems(username: String): Seq[String] = ???
+  def getItems(username: String): Seq[String] = {
+    items.get(username).getOrElse(Nil)
+  }
 
   def addItem(username: String, item: String): Unit = ???
 
